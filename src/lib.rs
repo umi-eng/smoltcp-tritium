d2605dfa8ee1eb2f87dd4440c9c33eb04aaf4b86
Liam Kinne <liam@kinne.io>
2024-02-19 01:42:55 +1000

    add bit flags

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,15 @@
 pub mod client;
 #[cfg(feature = "server")]
 pub mod server;
+
+/// Flags bitfield.
+pub(crate) struct Flags(u8);
+
+bitflags::bitflags! {
+    impl Flags: u8 {
+        const Heartbeat = 1 << 7;
+        const Settings = 1 << 6;
+        const FrameRtr = 1 << 1;
+        const FrameExtendedId = 1 << 0;
+    }
+}
